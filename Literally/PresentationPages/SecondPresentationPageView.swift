c52d0856919c1844f9b387dc04a33e22721454b5
Carloto11 <yasmincarlotobezerra17@gmail.com>
2023-06-13 14:55:25 -0300

    Presentation page

---
+++ Literally/PresentationPages/SecondPresentationPageView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondPresentationPageView.swift
+//  Literally
+//
+//  Created by user on 13/06/23.
+//
+
+import SwiftUI
+
+struct SecondPresentationPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondPresentationPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondPresentationPageView()
+    }
+}
