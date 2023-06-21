6194b907059feb5d5f27a8aefff6ff011c82648e
Daniel02md <daniel.marcos.bezerra@gmail.com>
2023-06-20 21:17:49 -0300

    Concertando conflitos

--- Literally/SearchPage/baseSearchView.swift
+++ Literally/SearchPage/baseSearchView.swift
@@ -11,54 +11,29 @@ struct baseSearchView: View {
     
     var movies = ["Jojo Rabbit", "Jonh Wick"]
     var books = ["É assim que acaba", "Estilhaca-me"]
-
-    @State private var searchText = ""
-
-        var body: some View {
-            NavigationStack {
-                
-                List{
- 
-                        NavigationLink {
-                            Text(name)
-                        } label: {
-                            Text(name)
-                        }
-                    }
-                }
-                
-            }
-            .searchScopes($scope) {
-                   Text("Filme").tag(ChoiceScope.movie)
-                   Text("Livro").tag(ChoiceScope.book)
-            }
-            
-            .searchable(text: $searchText)
-                        
-        }
-    
     enum ChoiceScope {
         case movie
         case book
     }
     
-    @State private var scope: ChoiceScope = .book
+    @State var scope: ChoiceScope = .book
+    @State private var searchText = ""
     
     var filteredMovies: [String] {
-            if searchText.isEmpty {
-                return movies
-            }
-            else {
-                return movies.filter { $0.contains(searchText) }
-            }
+        if searchText.isEmpty {
+            return movies
+        }
+        else {
+            return movies.filter { $0.contains(searchText) }
+        }
     }
     
     var filteredBooks: [String] {
-            if searchText.isEmpty {
-                return books
-            } else {
-                return books.filter { $0.contains(searchText) }
-            }
+        if searchText.isEmpty {
+            return books
+        } else {
+            return books.filter { $0.contains(searchText) }
+        }
     }
     
     func choice () -> Array<String> {
@@ -68,10 +43,35 @@ struct baseSearchView: View {
             return filteredMovies
         }
     }
-
+    
+    var body: some View {
+        NavigationStack {
+            
+            List{
+                
+                NavigationLink {
+                    Text("name")
+                } label: {
+                    Text("name")
+                }
+            }
+            
+            
+        }
+        .searchScopes($scope) {
+            Text("Filme").tag(ChoiceScope.movie)
+            Text("Livro").tag(ChoiceScope.book)
+        }
+        .searchable(text: $searchText)
+        
+        
+        
+       
+        
+        
+    }
+    
 }
-
-
 
 struct baseSearchView_Previews: PreviewProvider {
     static var previews: some View {
